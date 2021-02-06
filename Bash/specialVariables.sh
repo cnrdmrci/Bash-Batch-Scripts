602cf4e2e0d4d19745f55d67f62bfbe7a1a75ceb
cnrdmrci <canerdemirci.mail@gmail.com>
2021-02-06 14:50:30 +0300

    specialVariables

---
+++ Bash/specialVariables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#print current shell pid
+echo pid: $$
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+
+for TOKEN in $*
+do
+  echo $TOKEN
+done
+
