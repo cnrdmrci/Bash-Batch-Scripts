8c9785a03f34c5403ff2565e313d5045664db5a2
cnrdmrci <canerdemirci.mail@gmail.com>
2021-02-06 14:51:36 +0300

    operations

---
+++ Bash/operations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sum=`expr 2 + 2`
+sub=`expr 2 - 2`
+div=`expr 2 / 2`
+mul=`expr 2 \* 2`
+mod=`expr 2 % 2`
+
+echo "Sum: $sum"
+echo "Sub: $sub"
+echo "Div: $div"
+echo "Mul: $mul"
+echo "Mod: $mod"
+
+# -eq -> equal
+# -ne -> not equal
+# -gt -> greater than
+# -lt -> less than
+# -ge -> greater equal
+# -le -> less equal
+
+# ! -> not
+# -o -> or
+# -a -> and
+
